91a1501f4c94d43bd079d20c6cf127a3f72ef8f4
ancadunavete <anca.dnvt@gmail.com>
2025-01-17 18:28:07 +0100

    add: 2nd ex

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -1,13 +1,21 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+const letterNames = []
 
 
 // Dichiara la funzione qui.
 
+function letturaArray() {
+    for ( let i = 0; i < names.length; i++ )
+        console.log(names[i][0])
+        letterNames.push (names[i][0])
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+letturaArray()
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log (letterNames)
