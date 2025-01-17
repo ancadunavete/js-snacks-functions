416b618598817fc2fa209486cf894e372eef109d
ancadunavete <anca.dnvt@gmail.com>
2025-01-17 18:22:28 +0100

    add: 1st ex

--- snack-funzioni/01_saluti/script.js
+++ snack-funzioni/01_saluti/script.js
@@ -5,9 +5,12 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+function saluto (nome) {
+    console.log ( `Ciao ${nome}` )
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log (saluto (userName))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
